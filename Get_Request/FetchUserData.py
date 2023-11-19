8f2c5b2f4e09eac560e1c3f70938df1ac37d15e0
hp <srikanthag93@gmail.com>
2023-11-19 16:07:00 +0530

    API_Push_01

--- Get_Request/FetchUserData.py
+++ Get_Request/FetchUserData.py
@@ -9,11 +9,11 @@ Api_URL = "https://reqres.in/api/users?page=2"
 
 # # Send get requests
 response = requests.get(Api_URL)
-# print(response)
+print(response)
 
 # # Display response content
-# print(response.content)
-# print(response.headers)
+print(response.content)
+print(response.headers)
 
 # Parse  response to JSON
 JSON_Response = json.loads(response.text)
@@ -23,7 +23,7 @@ JSON_Response = json.loads(response.text)
 pages = jsonpath.jsonpath(JSON_Response, 'total_pages')
 # print(pages[0])
 assert pages[0] == 2    # Asser will  compare
-assert pages[0] == 5    # user will get  assertion error
+# assert pages[0] == 5    # user will get  assertion error
 
 
 
